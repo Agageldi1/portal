87374a93096ce0d599c14af8f00b93744bb28b46
Agageldi1 <86819756+Agageldi1@users.noreply.github.com>
2021-07-02 12:13:07 +0300

    portal1

---
+++ js/script.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+   $('.icon-menu').click(function (event) {
+      $('.icon-menu,.menu__body').toggleClass('active');
+   });
+});
+function ibg() {
+   let ibg = document.querySelectorAll(".ibg"); for (var i = 0; i < ibg.length; i++) { if (ibg[i].querySelector('img')) { ibg[i].style.backgroundImage = 'url(' + ibg[i].querySelector('img').getAttribute('src') + ')'; } }
+}
+ibg();
